0a8d78104d0a0cc2de3594533ac097cf88a900e9
sasikumar9951 <sasikumar.s9951@gmail.com>
2026-02-21 21:49:30 +0530

    latest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-const nextJest = require("next/jest");
+import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
   dir: "./",
@@ -9,4 +9,4 @@ const customJestConfig = {
   testEnvironment: "jsdom",
 };
 
-module.exports = createJestConfig(customJestConfig);
+export default createJestConfig(customJestConfig);
--- jest.setup.js
+++ jest.setup.js
@@ -1,12 +1,1 @@
-const nextJest = require("next/jest");
-
-const createJestConfig = nextJest({
-  dir: "./",
-});
-
-const customJestConfig = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  testEnvironment: "jsdom",
-};
-
-module.exports = createJestConfig(customJestConfig);
+import '@testing-library/jest-dom';
